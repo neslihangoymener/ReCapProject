b42653cc40cdb5d92333145d75aa4d60ed39e539
Neslihan Kuş Göymener <neslihangoymener@gmail.com>
2021-02-14 18:47:28 +0300

    11.gün ödev1

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+
+            //Dependency chain --
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
+        [HttpGet("GetAllByColorId")]
+
+        //https://localhost:44367/api/cars/GetAllByColorId?id=2
+        public IActionResult GetById(int id)
+        {
+            var result = _carService.GetAllByColorId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
+
