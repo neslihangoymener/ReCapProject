01844aeba77373dc226b8a39aa5026a19b319327
Neslihan Kuş Göymener <neslihangoymener@gmail.com>
2021-02-14 20:34:08 +0300

    11.gün ödev1.2

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -37,7 +37,7 @@ namespace WebAPI.Controllers
         [HttpGet("GetAllByColorId")]
 
         //https://localhost:44367/api/cars/GetAllByColorId?id=2
-        public IActionResult GetById(int id)
+        public IActionResult GetAllByColorId(int id)
         {
             var result = _carService.GetAllByColorId(id);
             if (result.Success)
@@ -46,11 +46,55 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+        [HttpGet("GetAllByModelYear")]
 
+        //https://localhost:44367/api/cars/GetAllByModelYear?year=2020
+        public IActionResult GetAllByModelYear(int year)
+        {
+            var result = _carService.GetAllByModelYear(year);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetAllByBrandId")]
+
+        //https://localhost:44367/api/cars/GetAllByBrandId?year=2020
+        public IActionResult GetAllByBrandId(int id)
+        {
+            var result = _carService.GetAllByBrandId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
         [HttpPost("add")]
         public IActionResult Add(Car car)
         {
             var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Car car)
+        {
+            var result = _carService.Update(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Car car)
+        {
+            var result = _carService.Delete(car);
             if (result.Success)
             {
                 return Ok(result);
